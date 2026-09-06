74d3b048de9288930be29f37e3fb5e0702311746
Eswar <dharanigk2018@gmail.com>
2026-09-06 19:14:48 +0530

    updated .env

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:8000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:8000",
+      process.env.DEPLOY_URL,
+    ],
   }),
 );
 app.use(morgan("dev"));
@@ -38,7 +42,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:8000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:8000",
+      process.env.DEPLOY_URL,
+    ],
     methods: ["GET", "POST"],
   },
 });
